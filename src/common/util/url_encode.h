8bb4497e032b89d3d204389c90d88bbbe0fd063f
flyonaie <504185859@qq.com>
2024-12-16 13:50:21 +0800

    1、对url_encode.h和url_parser.h进行中文注释； 2、版本号变更

--- src/common/util/url_encode.h
+++ src/common/util/url_encode.h
@@ -8,10 +8,23 @@
 
 namespace aimrt::common::util {
 
+/**
+ * @brief 将数字转换为十六进制字符
+ * 
+ * @param[in] x 要转换的数字(0-15)
+ * @param[in] up true表示转换为大写字母，false表示转换为小写字母
+ * @return unsigned char 转换后的十六进制字符
+ */
 inline unsigned char ToHex(unsigned char x, bool up) {
   return x > 9 ? x + (up ? 'A' : 'a') - 10 : (x + '0');
 }
 
+/**
+ * @brief 将十六进制字符转换为数字
+ * 
+ * @param[in] x 十六进制字符('0'-'9', 'A'-'F', 'a'-'f')
+ * @return unsigned char 转换后的数字(0-15)，如果输入无效则返回0
+ */
 inline unsigned char FromHex(unsigned char x) {
   if (x >= '0' && x <= '9') {
     return x - '0';
@@ -26,11 +39,17 @@ inline unsigned char FromHex(unsigned char x) {
 }
 
 /**
- * @brief UrlEncode
- *
- * @param[in] str string to be encoded
- * @param[in] up whether to encode to uppercase
- * @return std::string encoded string
+ * @brief URL编码，将字符串转换为URL安全的格式
+ * 
+ * 编码规则:
+ * 1. 字母数字保持不变
+ * 2. 特殊字符('-', '_', '.', '~')保持不变
+ * 3. 空格转换为'+'
+ * 4. 其他字符转换为%XX格式，XX为字符的十六进制ASCII码
+ * 
+ * @param[in] str 需要编码的字符串
+ * @param[in] up 是否使用大写十六进制字母，默认为true
+ * @return std::string 编码后的字符串
  */
 inline std::string UrlEncode(std::string_view str, bool up = true) {
   std::string ret_str;
@@ -51,10 +70,15 @@ inline std::string UrlEncode(std::string_view str, bool up = true) {
 }
 
 /**
- * @brief UrlDecode
- *
- * @param[in] str string to be decoded
- * @return std::string decoded string
+ * @brief URL解码，将URL编码的字符串转换回原始格式
+ * 
+ * 解码规则:
+ * 1. '+'转换为空格
+ * 2. %XX格式转换为对应的字符，XX为十六进制ASCII码
+ * 3. 其他字符保持不变
+ * 
+ * @param[in] str 需要解码的字符串
+ * @return std::string 解码后的字符串
  */
 inline std::string UrlDecode(std::string_view str) {
   std::string ret_str;
@@ -78,6 +102,18 @@ inline std::string UrlDecode(std::string_view str) {
   return ret_str;
 }
 
+/**
+ * @brief HTTP头部编码，将字符串转换为HTTP头部安全的格式
+ * 
+ * 编码规则:
+ * 1. 字母数字保持不变
+ * 2. 连字符'-'保持不变
+ * 3. 其他字符转换为%XX格式，XX为字符的十六进制ASCII码
+ * 
+ * @param[in] str 需要编码的字符串
+ * @param[in] up 是否使用大写十六进制字母，默认为true
+ * @return std::string 编码后的字符串
+ */
 inline std::string HttpHeaderEncode(std::string_view str, bool up = true) {
   std::string ret_str;
   ret_str.reserve(str.length() * 3);
@@ -94,6 +130,16 @@ inline std::string HttpHeaderEncode(std::string_view str, bool up = true) {
   return ret_str;
 }
 
+/**
+ * @brief HTTP头部解码，将HTTP头部编码的字符串转换回原始格式
+ * 
+ * 解码规则:
+ * 1. %XX格式转换为对应的字符，XX为十六进制ASCII码
+ * 2. 其他字符保持不变
+ * 
+ * @param[in] str 需要解码的字符串
+ * @return std::string 解码后的字符串
+ */
 inline std::string HttpHeaderDecode(std::string_view str) {
   std::string ret_str;
   ret_str.reserve(str.length());
--- src/common/util/url_parser.h
+++ src/common/util/url_parser.h
@@ -11,25 +11,28 @@
 namespace aimrt::common::util {
 
 /**
- * @brief url元素
- *
- * @tparam StringType
+ * @brief URL结构体，用于存储URL的各个组成部分
+ * @details 将URL分解为协议、主机名、端口、路径、查询参数和片段等组成部分
+ * 
+ * @tparam StringType 字符串类型，默认为std::string
  */
 template <class StringType = std::string>
 struct Url {
-  StringType protocol;  // 协议
-  StringType host;      // host
-  StringType service;   // 端口
-  StringType path;      // 路径
-  StringType query;     // 参数
-  StringType fragment;  // 额外信息
+  StringType protocol;  // 协议，如http、https等
+  StringType host;      // 主机名，如www.example.com
+  StringType service;   // 服务端口，如80、443等
+  StringType path;      // 资源路径，如/path/to/resource
+  StringType query;     // 查询参数，如key1=value1&key2=value2
+  StringType fragment;  // URL片段标识符，以#开头的部分
 };
 
 /**
- * @brief url解析
- * @note url结构：[protocol://][host][:service][path][?query][#fragment]
- * @param url_str url字符串
- * @return std::optional<UrlView> url结构，nullopt则代表解析失败
+ * @brief 解析URL字符串为URL结构体
+ * @details 使用正则表达式将URL字符串解析为各个组成部分，支持标准URL格式
+ * 
+ * @tparam StringType 字符串类型，默认为std::string
+ * @param url_str 需要解析的URL字符串
+ * @return std::optional<Url<StringType>> 解析成功返回URL结构体，失败返回std::nullopt
  */
 template <class StringType = std::string>
 std::optional<Url<StringType>> ParseUrl(const std::string& url_str) {
@@ -65,10 +68,12 @@ std::optional<Url<StringType>> ParseUrl(const std::string& url_str) {
 }
 
 /**
- * @brief url拼接
- * @note url结构：[protocol://][host][:service][path][?query][#fragment]
- * @param url url结构
- * @return std::string url字符串
+ * @brief 将URL结构体转换为URL字符串
+ * @details 根据URL结构体中的各个组成部分，按照标准URL格式拼接成完整的URL字符串
+ * 
+ * @tparam StringType 字符串类型，默认为std::string
+ * @param url URL结构体，包含URL的各个组成部分
+ * @return std::string 拼接后的完整URL字符串
  */
 template <class StringType = std::string>
 std::string JoinUrl(const Url<StringType>& url) {
